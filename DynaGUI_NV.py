c88c5bd33911a2adde9e14c3a4839491b7186b64
benjaminbolling <benjaminbolling@esss.se>
2019-11-01 08:44:31 +0100

    Added GNU GPL 3.0 to all .py-files.

--- DynaGUI_NV.py
+++ DynaGUI_NV.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 20 15:59:21 2019
+<A Dynamic Graphical User Interface package, which gives users a method to construct temporary, permanent and/or a set of GUI:s for users in a simple and fast manner combined with diagnostics tools (with advance 1D and 2D plotting methods).>
+    Copyright (C) <2019>  <Benjamin Edward Bolling>
 
-@author: controlroom [benjamin bolling]
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-DynaGUI NV (Numerical Values)
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 try:
     import PyQt5.QtWidgets as QtGui
